c1a3c29a2ca18b218a009b1ec1cac6215e797dd1
ym <158552>
2019-07-01 20:45:51 +0800

    添加user类的ID属性

---
+++ src/main/java/ym/springRedis/servlet/MyServlet.java
@@ -0,0 +1,19 @@
+package ym.springRedis.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+public class MyServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request,response);
+    }
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("..............") ;
+        request.getRequestDispatcher("/hello.jsp").forward(request,response);
+    }
+}
